8e404aec4d1af0cd50976a14cb4d17acef102e2d
B-L-Lehner <lehnerbela@gmail.com>
2020-12-13 17:02:39 +0100

    Javitva a beolvasas, de nem mukodik minden osztalyra, mert ayoknak a metodusai uresek. Az osztaly metodusokat meg kell csinalni!!!

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,19 @@
 import sun.rmi.server.LoaderHandler;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // write your code here
-
-
+        Parkolo p = new Parkolo();
+        p.beolvasFile("jarmuvek.txt");
+        System.out.println(p.getFerohelyAuto());
+        System.out.println((p.getJarmu(1)).getRendszam());
+        System.out.println((p.getJarmuRendszam("AAA-123")).getClass());
     }
 }
 
-
-// Ez itt csak a teszteleshez alkalmazott kod lett...
-
-//        LocalDateTime be = LocalDateTime.now();
-//        LocalDateTime ki = LocalDateTime.now();
-//
-//        System.out.println(be.toString());
-//        System.out.println(ki.toString());
-//
-//        Jarmu fabia = new Auto("abc-123", be, ki, false);
-//        Jarmu jazz = new Auto("abc-234", LocalDateTime.now(), LocalDateTime.now(), false);
-//        Jarmu vespa = new Motor("abc-234", LocalDateTime.now(), LocalDateTime.now(), false);
-//
-//        System.out.println("A harom osztaly pontos meghatarozasa: ");
-//        System.out.println(fabia.getClass());
-//        System.out.println(jazz.getClass());
-//        System.out.println(vespa.getClass());
-//
-//
-//        System.out.println("Ekkor lepett ki a Fabia:  " + fabia.getKilepesiIdo().toString());
-//        System.out.println("Ekkor lepett ki a Jazz:  " + jazz.getKilepesiIdo().toString());
-//
-//        ArrayList<Jarmu> tarolo = new ArrayList<>();
-//
-//        tarolo.add(fabia);
-//        tarolo.add(jazz);
-//
-//        System.out.println("Ez az egyes auto rendszama:  " + tarolo.get(0).getRendszam());
-//        System.out.println("Ez az kettes auto rendszama:  " + tarolo.get(1).getRendszam());
-//
-//        //PL. torles a tarolobol (vagyis kilepes)
-//
-//        for (int i = 0; i < tarolo.size(); i++) {
-//            if (tarolo.get(i).getRendszam().equals("abc-234")) {
-//                tarolo.remove(i);
-//            }
-//        }
-//
-//        System.out.println("A tarolo merete: " + tarolo.size());
-//
-//
-//    }
-//}
---
+++ src/Parkolo.java
@@ -0,0 +1,115 @@
+import sun.security.krb5.internal.crypto.Aes128;
+
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Parkolo {
+    private int ferohelyMotor = 25;
+    private int ferohelyAuto = 120;
+    private int ferohelyTeherauto = 20;
+    private ArrayList<Automata> automataTarolo = new ArrayList<>();
+    private ArrayList<Jarmu> jarmuTarolo = new ArrayList<>();
+    LocalDateTime ido;
+
+    public int getFerohelyMotor() {
+        return ferohelyMotor;
+    }
+
+    public void setFerohelyMotor(int ferohelyMotor) {
+        this.ferohelyMotor = ferohelyMotor;
+    }
+
+    public int getFerohelyAuto() {
+        return ferohelyAuto;
+    }
+
+    public void setFerohelyAuto(int ferohelyAuto) {
+        this.ferohelyAuto = ferohelyAuto;
+    }
+
+    public int getFerohelyTeherauto() {
+        return ferohelyTeherauto;
+    }
+
+    public void setFerohelyTeherauto(int ferohelyTeherauto) {
+        this.ferohelyTeherauto = ferohelyTeherauto;
+    }
+
+    public LocalDateTime getIdo() {
+        return ido;
+    }
+
+    public void setIdo(LocalDateTime ido) {
+        this.ido = ido;
+    }
+
+    public Jarmu getJarmuRendszam(String rendszam) {
+
+            for (int i = 0; i < this.jarmuTarolo.size(); i++) {
+                if (this.jarmuTarolo.get(i).getRendszam().equals(rendszam.trim())) {
+                    return this.jarmuTarolo.get(i);
+                }
+            }
+            return null;
+        }
+
+
+
+    public Jarmu getJarmu(int szam) {
+        return this.jarmuTarolo.get(szam);
+    }
+
+
+
+    //készítettem egy beolvasást tesztelve müködik!
+    public void beolvasFile(String fajlNev) throws IOException {
+        File fajl = new File(fajlNev);
+        try (FileReader fr = new FileReader(fajl);
+             BufferedReader br = new BufferedReader(fr)) {
+
+            String sorok;
+            //ArrayList<Jarmu> tarolo = new ArrayList<>();
+
+            while ((sorok = br.readLine()) != null) {
+                String[] szetvagSorok = sorok.split(";");
+
+                switch (szetvagSorok[0]) {
+                    case "auto":
+                        Jarmu auto = new Auto(szetvagSorok[1].trim(), LocalDateTime.parse(szetvagSorok[2]), LocalDateTime.parse(szetvagSorok[3]), false);
+                        this.jarmuTarolo.add(auto);
+                        this.setFerohelyAuto(getFerohelyAuto() - 1);
+                        break;
+                    case "motor":
+                        Jarmu motor = new Motor(szetvagSorok[1].trim(), LocalDateTime.parse(szetvagSorok[2]), LocalDateTime.parse(szetvagSorok[3]), false);
+                        this.jarmuTarolo.add(motor);
+                        this.setFerohelyMotor(getFerohelyMotor() - 1);
+                        break;
+                    case "teherauto":
+                        Jarmu teher = new Teherauto(szetvagSorok[1].trim(), LocalDateTime.parse(szetvagSorok[2]), LocalDateTime.parse(szetvagSorok[3]), false);
+                        this.jarmuTarolo.add(teher);
+                        this.setFerohelyTeherauto(getFerohelyTeherauto() - 1);
+                        break;
+                }
+            }
+
+        }
+    }
+
+    public boolean belepJarmu() {
+        return true;
+    }
+
+    public boolean kilepJarmu() {
+        return true;
+    }
+
+    public void hozzaadAutomata() {
+
+    }
+
+    public void kiveszAutomata() {
+
+    }
+}
